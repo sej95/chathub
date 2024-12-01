c0b7c9ce12c34bc3f503635ec66e16c8f42a2d29
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-02 07:52:22 +0800

    Update Client.tsx

--- src/app/loading/Client.tsx
+++ src/app/loading/Client.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 import FullscreenLoading from '@/components/FullscreenLoading';
 
 const Loading = () => {
-  const { t } = useTranslation('common');
+//  const { t } = useTranslation('common');
 
   return <FullscreenLoading title={t('appInitializing')} />;
 };
