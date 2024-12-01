d0250ebce48f2ea8df76f61e3b9f08ab469679cb
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-01 21:38:23 +0800

    Add files via upload

--- src/features/Follow/index.tsx
+++ src/features/Follow/index.tsx
@@ -48,14 +48,15 @@ const Follow = memo(() => {
           title={t('follow', { name: 'Discord' })}
         />
       </Link>
-      <Link href={MEDIDUM} rel="noreferrer" target={'_blank'}>
-        <ActionIcon
-          className={styles.icon}
-          icon={SiMedium as any}
-          title={t('follow', { name: 'Discord' })}
-        />
-      </Link>
-    </Flexbox>
+/**      <Link href={MEDIDUM} rel="noreferrer" target={'_blank'}>
+   *     <ActionIcon
+    *      className={styles.icon}
+     *     icon={SiMedium as any}
+      *    title={t('follow', { name: 'medidum' })}
+       * />
+*      </Link>
+*/ 
+ </Flexbox>
   );
 });
 
