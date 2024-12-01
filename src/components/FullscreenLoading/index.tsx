13b20a77a6c2fb696a05e74640f51a636163127f
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-02 07:33:57 +0800

    Add files via upload

--- src/components/FullscreenLoading/index.tsx
+++ src/components/FullscreenLoading/index.tsx
@@ -11,7 +11,7 @@ const FullscreenLoading = memo<{ title?: string }>(({ title }) => {
       <Center flex={1} gap={16} width={'100%'}>
         <ProductLogo size={48} type={'combine'} />
         <Center gap={12} horizontal style={{ fontSize: 15, lineHeight: 1.5, opacity: 0.66 }}>
-          <Icon icon={Loader2} size={{ fontSize: 160 }} spin />
+          <Icon icon={Loader2} size={{ fontSize: 16 }} spin />
           {title}
         </Center>
       </Center>
