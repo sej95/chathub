449d811685af8279163a09a86691e85387e16dfa
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-02 07:03:28 +0800

    Update index.tsx

--- src/components/FullscreenLoading/index.tsx
+++ src/components/FullscreenLoading/index.tsx
@@ -9,7 +9,7 @@ const FullscreenLoading = memo<{ title?: string }>(({ title }) => {
   return (
     <Flexbox height={'100%'} style={{ position: 'relative', userSelect: 'none' }} width={'100%'}>
       <Center flex={1} gap={16} width={'100%'}>
-        <ProductLogo size={48} type={'combine'} />
+        <ProductLogo size={248} type={'combine'} />
         <Center gap={12} horizontal style={{ fontSize: 15, lineHeight: 1.5, opacity: 0.66 }}>
           <Icon icon={Loader2} size={{ fontSize: 16 }} spin />
           {title}
