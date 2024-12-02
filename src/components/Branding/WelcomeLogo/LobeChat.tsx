a681a2e5c046f0b8ab6ad3f5666986c2a0f5783a
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-02 09:35:51 +0800

    Update LobeChat.tsx

--- src/components/Branding/WelcomeLogo/LobeChat.tsx
+++ src/components/Branding/WelcomeLogo/LobeChat.tsx
@@ -9,13 +9,13 @@ const LogoSpline = dynamic(() => import('@lobehub/ui/es/LogoThree/LogoSpline'), 
 
 const WelcomeLogo = memo<{ mobile?: boolean }>(({ mobile }) => {
   return mobile ? (
-    <Center height={240} width={240}>
+    <Center height={440} width={240}>
       <LogoThree size={240} />
     </Center>
   ) : (
     <Center
       style={{
-        height: `min(482px, 40vw)`,
+        height: `min(882px, 40vw)`,
         marginBottom: '-10%',
         marginTop: '-20%',
         position: 'relative',
