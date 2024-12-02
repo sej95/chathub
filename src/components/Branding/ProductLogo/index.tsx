ae7e882c0e034c46c485e436594e2bea1de253fa
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-02 13:59:19 +0800

    Update index.tsx

--- src/components/Branding/ProductLogo/index.tsx
+++ src/components/Branding/ProductLogo/index.tsx
@@ -4,12 +4,12 @@ import { memo } from 'react';
 import { isCustomBranding } from '@/const/version';
 
 import CustomLogo from './Custom';
-import ChatHubLogo from './ChatHub';
+//import ChatHubLogo from './ChatHub';
 
 export const ProductLogo = memo<LobeChatProps>((props) => {
   if (isCustomBranding) {
     return <CustomLogo {...props} />;
   }
 
-  return <ChatHubLogo {...props} />;
+  return <LobeHubLogo {...props} />;
 });
