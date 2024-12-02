c17da07b7435ae0a9bda2f51ef4a38a8f5adee7c
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-03 07:48:29 +0800

    Update index.tsx

--- src/components/BrandWatermark/index.tsx
+++ src/components/BrandWatermark/index.tsx
@@ -38,7 +38,7 @@ const BrandWatermark = memo<Omit<FlexboxProps, 'children'>>(({ style, ...rest })
       ) : (
         <Link
           className={styles.logoLink}
-          href={`https://chat-top.vercel.app?utm_source=${UTM_SOURCE}&utm_content=brand_watermark`}
+          href={`https://lobehub.com?utm_source=${UTM_SOURCE}&utm_content=brand_watermark`}
           target={'_blank'}
         >
           <Sej95 size={20} type={'text'} />
