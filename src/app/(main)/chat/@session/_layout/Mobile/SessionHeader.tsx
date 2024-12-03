53f24dfe518f00ea5e971fb73e284ffed408492a
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-03 20:24:49 +0800

    Update SessionHeader.tsx

--- src/app/(main)/chat/@session/_layout/Mobile/SessionHeader.tsx
+++ src/app/(main)/chat/@session/_layout/Mobile/SessionHeader.tsx
@@ -24,7 +24,7 @@ const Header = memo(() => {
       left={
         <Flexbox align={'center'} gap={8} horizontal style={{ marginLeft: 8 }}>
           <UserAvatar onClick={() => router.push('/me')} size={32} />
-          <ProductLogo type={'text'} />
+          <ProductLogo size={0} type={'text'} />
           {enableWebrtc && <SyncStatusInspector placement={'bottom'} />}
         </Flexbox>
       }
