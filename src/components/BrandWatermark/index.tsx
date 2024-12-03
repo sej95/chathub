c3c6c741136ba83e586971e615ecc3a2a6007622
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-03 10:14:59 +0800

    Update index.tsx

--- src/components/BrandWatermark/index.tsx
+++ src/components/BrandWatermark/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { LobeChat } from '@lobehub/ui/brand';
+import { LobeHub } from '@lobehub/ui/brand';
 import { createStyles } from 'antd-style';
 import Link from 'next/link';
 import { memo } from 'react';
@@ -41,7 +41,7 @@ const BrandWatermark = memo<Omit<FlexboxProps, 'children'>>(({ style, ...rest })
           href={`https://lobehub.com?utm_source=${UTM_SOURCE}&utm_content=brand_watermark`}
           target={'_blank'}
         >
-          <LobeChat size={20} type={'text'} />
+          <LobeHub size={20} type={'text'} />
         </Link>
       )}
     </Flexbox>
