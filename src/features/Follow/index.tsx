0a77d3ca7c75f0b3d8f39aaeff76293c42763d2e
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-03 23:21:01 +0800

    Update index.tsx

--- src/features/Follow/index.tsx
+++ src/features/Follow/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { SiDiscord, SiGithub, SiBaidu, SiMedium, SiX } from '@icons-pack/react-simple-icons';
+import { SiDiscord, SiGithub, SiMedium, SiX } from '@icons-pack/react-simple-icons';
 import { ActionIcon } from '@lobehub/ui';
 import { createStyles } from 'antd-style';
 import Link from 'next/link';
