602446844cc4f0126862ee0b5454021e8016bd4d
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-05 19:49:20 +0800

    Update index.tsx

--- src/features/Follow/index.tsx
+++ src/features/Follow/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { SiDiscord, SiGithub, SiMedium, SiX } from '@icons-pack/react-simple-icons';
+import { SiDiscord, SiGithub, SiMedium, SiReact } from '@icons-pack/react-simple-icons';
 import { ActionIcon } from '@lobehub/ui';
 import { createStyles } from 'antd-style';
 import Link from 'next/link';
@@ -39,7 +39,7 @@ const Follow = memo(() => {
         />
       </Link>
       <Link href={X} rel="noreferrer" target={'_blank'}>
-        <ActionIcon className={styles.icon} icon={SiX as any} title={t('follow', { name: 'X' })} />
+        <ActionIcon className={styles.icon} icon={SiReact as any} title={t('follow', { name: 'X' })} />
       </Link>
       <Link href={DISCORD} rel="noreferrer" target={'_blank'}>
         <ActionIcon
