f457799b8975f74a9f45695132b3a3da608c2992
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-05 09:51:35 +0800

    Update selectors.test.ts

--- src/store/user/slices/auth/selectors.test.ts
+++ src/store/user/slices/auth/selectors.test.ts
@@ -84,7 +84,7 @@ describe('userProfileSelectors', () => {
         enableAuth: () => false,
       } as unknown as UserStore;
 
-      expect(userProfileSelectors.username(store)).toBe('ChatHub');
+      expect(userProfileSelectors.username(store)).toBe('LobeChat');
     });
 
     it('should return user username when signed in', () => {
